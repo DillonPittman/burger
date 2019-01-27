96ef8713dd38940acf6fde128bc0a3a423454b52
DillonPittman <pittmanwdillon@gmail.com>
2019-01-27 15:00:01 -0600

    initialized and added schema, seed, config

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('New Bacon-ings');
+INSERT INTO burgers (burger_name) VALUES ('The Child Molester');
+INSERT INTO burgers (burger_name) VALUES ('Foot Feta-ish Burger');
